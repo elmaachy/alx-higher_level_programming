88204080964abb3df335e2da35cbef61df7c3a44
elmaachy <elmaachy@gmail.com>
2023-08-11 14:13:40 -0700

    a

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,7 @@
-import add_0
-if __name__ == '__main__':
-    print(add_0.add(1,2)
+#!/usr/bin/python3
+from add_0 import add
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
